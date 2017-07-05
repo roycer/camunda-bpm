27174fa2cf1bbd4e8aa18a15636151c5262456fc
Roycer <Roycer@DESKTOP-E87A7G6>
2017-07-05 12:08:48 -0500

    logger info

--- src/main/java/com/sig/girado/MyProcessApplication.java
+++ src/main/java/com/sig/girado/MyProcessApplication.java
@@ -1,16 +1,58 @@
 package com.sig.girado;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ExternalTaskService;
+import org.camunda.bpm.engine.FilterService;
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.ManagementService;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
 
 @ProcessApplication
 public class MyProcessApplication extends ServletProcessApplication {
 
-	  @PostDeploy
-	  public void onDeploymentFinished(ProcessEngine processEngine) {
+	private static Logger LOGGER = Logger.getLogger(MyProcessApplication.class.getName());
 
-	    System.out.println("hola mundo desde CamundaBPMProcessApplication");
-	  }
+	@PostDeploy
+	public void onDeploymentFinished(ProcessEngine processEngine) {
+
+		LOGGER.log(Level.INFO, "\nGirado: " + processEngine.getName());
+
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		TaskService taskService = processEngine.getTaskService();
+		IdentityService identityService = processEngine.getIdentityService();
+		FormService formService = processEngine.getFormService();
+		HistoryService historyService = processEngine.getHistoryService();
+		ManagementService managementService = processEngine.getManagementService();
+		FilterService filterService = processEngine.getFilterService();
+		ExternalTaskService externalTaskService = processEngine.getExternalTaskService();
+		CaseService caseService = processEngine.getCaseService();
+		DecisionService decisionService = processEngine.getDecisionService();
+
+		LOGGER.log(Level.INFO, repositoryService.toString());
+		LOGGER.log(Level.INFO, repositoryService.toString());
+		LOGGER.log(Level.INFO, runtimeService.toString());
+		LOGGER.log(Level.INFO, taskService.toString());
+		LOGGER.log(Level.INFO, identityService.toString());
+		LOGGER.log(Level.INFO, formService.toString());
+		LOGGER.log(Level.INFO, historyService.toString());
+		LOGGER.log(Level.INFO, managementService.toString());
+		LOGGER.log(Level.INFO, filterService.toString());
+		LOGGER.log(Level.INFO, externalTaskService.toString());
+		LOGGER.log(Level.INFO, caseService.toString());
+		LOGGER.log(Level.INFO, decisionService.toString());
+
+	}
 	}
--- src/main/java/com/sig/servlet/StartProcess.java
+++ src/main/java/com/sig/servlet/StartProcess.java
@@ -1,10 +1,11 @@
 package com.sig.servlet;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,41 +14,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
-import org.camunda.bpm.engine.variable.Variables;
-import org.camunda.bpm.engine.variable.value.ObjectValue;
-import org.camunda.spin.json.SpinJsonNode;
 import org.camunda.spin.plugin.variable.SpinValues;
 import org.camunda.spin.plugin.variable.value.JsonValue;
-
-import static org.camunda.spin.Spin.*;
-import static org.camunda.spin.DataFormats.*;
 
 /**
  * Servlet implementation class StartProcess
  */
 public class StartProcess extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-    
+	private static Logger LOGGER = Logger.getLogger(StartProcess.class.getName());
 	Properties propiedades = new Properties();
 	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public StartProcess() {
         super();
         try {
         	propiedades.load(getClass().getResourceAsStream("/camunda.properties"));
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e.getMessage());
 		}
-        
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String idBPMN = propiedades.getProperty("idBPMN");
@@ -64,14 +51,11 @@ public class StartProcess extends HttpServlet {
 	    JsonValue jsonValue = SpinValues.jsonValue(json).create();
 	    
 	    variables.put("expediente", jsonValue);
-		processEngine.getRuntimeService().startProcessInstanceByKey(idBPMN,variables);
+		String idProcessInstance = processEngine.getRuntimeService().startProcessInstanceByKey(idBPMN,variables).getProcessInstanceId();
+		LOGGER.log(Level.INFO,"idBPMN:"+idBPMN+"\nidProcessInstance: "+idProcessInstance+"\nmonto:"+monto+"\nnumero: "+numero);
 		
-		System.out.println("idBPMN:"+idBPMN+"\tmonto:"+monto+"\tnumero: "+numero);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
